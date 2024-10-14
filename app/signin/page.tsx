53d56ca0e1f43c88c2c0c8524ed1a5a8031998f9
CodeName-Sigma-M <purushothaman.v19@gmail.com>
2024-10-14 12:32:54 -0700

    commit home page

---
+++ app/signin/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+function Signin() {
+    return <div>
+        <h1>you are at signin</h1>;
+    <Link href="/">home</Link>
+        </div>
+}
+export default Signin;
